9c1a7b9bbfdda7005204f525131713cc6687d08b
sunshibao <sunshibao@laoyuegou.com>
2020-06-19 18:09:36 +0800

    init

---
+++ src/Cron/FieldFactoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace EasyTask\Cron;
+
+/**
+ * Interface FieldFactoryInterface
+ * @package EasyTask\Cron
+ */
+interface FieldFactoryInterface
+{
+    public function getField($position);
+}
